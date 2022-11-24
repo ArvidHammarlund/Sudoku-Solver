2f68902ee3fa5e2fa9926cd45ed4dc5e95fab9b5
Arvid Hammarlund <arvid.hammarlund@gmail.com>
2022-11-24 18:15:57 +0100

    gui first take

--- adapters/view.py
+++ adapters/view.py
@@ -1,6 +1,8 @@
 import json
 
 from abc import ABC, abstractmethod
+from tkinter import *
+from tkmacosx import * 
 
 class View(ABC):
     @abstractmethod
@@ -14,6 +16,59 @@ class View(ABC):
     @abstractmethod
     def current_board(self, board):
         pass
+
+
+class ViewGUI(object):
+    def __init__(self, board):
+        self.__root = Tk()
+        self.__board = board
+        self.__buttons = self.__build(board)
+        self.__current_digit = 0
+        mainloop()
+
+    def welcome_msg(self):
+        pass
+
+    def win_msg(self):
+        pass
+
+    def __build(self, board):
+        buttons = []
+        for i, row in enumerate(board):
+            for j, tile in enumerate(row):
+                btn = Button(self.__root,
+                             text=tile,
+                             bg='grey',
+                             width = 50,
+                             height = 50)
+                btn["command"] = lambda btn=btn: self.onClick(btn)
+                buttons.append(btn)
+                btn.grid(row=i, column=j)
+
+        for i in range(1,10):
+            btn = Button(self.__root,
+                    text=i,
+                    width=50,
+                    height=50,
+                    command= lambda i=i: self.command_click(i))
+            btn.grid(row=11, column=i - 1) 
+        return buttons
+
+    def command_click(self, n):
+        self.__current_digit = n
+        self.update(self.__board.parse(), self.__board.valid_assignments(n))
+
+    def update(self, values, valids):
+        for row, digits, marks in zip(self.__buttons, values, valids):
+            for btn, digit, mark in zip(row, digits, marks):
+                btn['text'] = digit
+                btn['bg'] = 'red'
+
+    def onClick(self, btn):
+        btn["text"] = self.__current_digit
+        btn["bg"] = "red"
+
+            
 
 class ViewCLI(View):
     def welcome_msg(self):
@@ -56,6 +111,7 @@ class ViewCLI(View):
             res += row
             res += "\n   +---+---+---+---+---+---+---+---+---+\n"
         print(res)
+
 
 
 
